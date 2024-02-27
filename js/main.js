7cf02fb1531431dfb4c71e28304e7aff979088b7
Web Design <idgwebdesign@gmail.com>
2024-02-27 11:49:57 -0300

    Cambios

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){ 
     
 
-
+  AOS.init({
+    once: true,
+    anchorPlacement: 'bottom-bottom'
+  });
+  
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         direction: 'horizontal',
